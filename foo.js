f63160955e5a205c4d3523463a4d0cd5505852b3
loonsamuel <44387515+loonsamuel@users.noreply.github.com>
2022-06-04 22:06:00 +0800

    Add files via upload

---
+++ foo.js
@@ -0,0 +1,86 @@
+// let spotsArr = [[0,'H1'],[0,'H2'],['F1',0],[0,0],[0,'F2'],['C',0]]
+const rooms = [['none', 'none'], ['none', 'none'], ['none', 'none'], ['none', 'none'], ['none', 'none'], ['none', 'none']];
+
+// const objcts = ['C', 'F', 'F', 'H1', 'H2'];
+const objcts = ['#000', '#d96b18', '#d96b18', '#0c7805', '#04438f'];
+
+
+function Randomize() {
+    //shuffle objects
+    var tmpobj = JSON.parse(JSON.stringify(objcts));
+    var tmpRooms = JSON.parse(JSON.stringify(rooms));
+
+    let shuffledObj = shuffle(tmpobj);
+    console.log(shuffledObj);
+
+    let randId = Math.floor(Math.random() * 6);
+
+    for (let i = 0; i < 6; i += 1) {
+        if (i !== randId) {
+            tmpRooms[i][Math.floor(Math.random() * 2)] = shuffledObj.pop();
+        }
+    }
+    // document.getElementById("red1").innerHTML = rooms[0][0];
+    // document.getElementById("red2").innerHTML = rooms[0][1];
+    // document.getElementById("yellow1").innerHTML = rooms[1][0];
+    // document.getElementById("yellow2").innerHTML = rooms[1][1];
+    // document.getElementById("green1").innerHTML = rooms[2][0];
+    // document.getElementById("green2").innerHTML = rooms[2][1];
+    // document.getElementById("brown1").innerHTML = rooms[3][0];
+    // document.getElementById("brown2").innerHTML = rooms[3][1];
+    // document.getElementById("white1").innerHTML = rooms[4][0];
+    // document.getElementById("white2").innerHTML = rooms[4][1];
+    // document.getElementById("blue1").innerHTML = rooms[5][0];
+    // document.getElementById("blue2").innerHTML = rooms[5][1];
+    // console.log(rooms);
+    // console.log(objcts);
+    // console.log(shuffledObj);
+    // console.log(tmpobj);
+    // console.log(tmpRooms);
+
+    var flattenArr = [];
+    for (var i = 0; i < tmpRooms.length; i++) {
+        flattenArr = flattenArr.concat(tmpRooms[i]);
+    }
+    // console.log(flattenArr);
+
+    resetColour();
+
+    $(".container .spot").each(function (index) {
+        console.log(flattenArr[index]);
+        $(this).css("background-color", flattenArr[index]); //reset
+        // d96b18
+    });
+
+
+}
+
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+function resetColour() {
+    $(".container .spot").each(function (index) {
+        $(this).css("background-color", "#c9c8c5"); //reset
+        // d96b18
+    });
+
+}
+// function includesObj(array) {
+//     return array.includes('C') || array.includes('F') || array.includes('H');
+// }
